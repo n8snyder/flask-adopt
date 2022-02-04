5e012d068840f0399b84be7ec23fa18266b21213
prokid20000 <95936374+prokid20000@users.noreply.github.com>
2022-02-04 18:18:01 -0500

    done with pet_add validators

--- forms.py
+++ forms.py
@@ -1,15 +1,16 @@
 """Forms for adopt app."""
 
+from wsgiref.validate import validator
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, TextAreaField
-
+from wtforms.validators import InputRequired, AnyOf, URL, Optional
 
 class AddPetForm(FlaskForm):
     """Form for adding pets."""
 
-    name = StringField("Name")
-    species = StringField("Species")
-    photo_url = StringField("Photo URL")
+    name = StringField("Name", validators=[InputRequired()])
+    species = StringField("Species", validators=[InputRequired(), AnyOf(["cat", "dog", "porcupine"], "Invalid species")])
+    photo_url = StringField("Photo URL", validators=[Optional(), URL(message = "Not a url")])
     age = SelectField(
         "Age",
         choices=[
@@ -18,5 +19,6 @@ class AddPetForm(FlaskForm):
             ("adult", "Adult"),
             ("senior", "Senior"),
         ],
+        validators = [InputRequired(), AnyOf(["baby","young","adult","senior"])]
     )
     notes = TextAreaField("Notes")
