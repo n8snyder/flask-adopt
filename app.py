82b7552da07576cfc58dbf61f31b8b4c58cc63a9
Nate Snyder <n8.snyder@berkeley.edu>
2022-02-04 11:35:31 -0800

    Add model for pet and start on homepage

--- app.py
+++ app.py
@@ -1,24 +1,30 @@
 """Flask app for adopt app."""
 
-from flask import Flask
+from flask import Flask, render_template
 
 from flask_debugtoolbar import DebugToolbarExtension
 
-from models import db, connect_db
+from models import db, connect_db, Pet
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = "secret"
+app.config["SECRET_KEY"] = "secret"
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///adopt"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///adopt"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 
 connect_db(app)
 db.create_all()
 
-# Having the Debug Toolbar show redirects explicitly is often useful;
-# however, if you want to turn it off, you can uncomment this line:
-#
-# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 toolbar = DebugToolbarExtension(app)
+
+
+@app.get("/")
+def homepage():
+    """Homepage which lists pets"""
+
+    pets = Pet.query.all()
+    return render_template("pet_list.html", pets=pets)
